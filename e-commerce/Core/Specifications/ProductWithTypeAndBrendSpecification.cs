2aedc53ead5c73aa2da0afadd3544f15a8772a44
yousef547 <54797102+yousef547@users.noreply.github.com>
2023-09-19 00:18:40 +0300

    complate part of `Section 4: API Generic Repository`

---
+++ e-commerce/Core/Specifications/ProductWithTypeAndBrendSpecification.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class ProductWithTypeAndBrendSpecification:BaseSpecification<Products>
+    {
+        public ProductWithTypeAndBrendSpecification()
+        {
+            AddIncludes(x => x.ProductType);
+            AddIncludes(x => x.ProductBrand);
+        }
+        public ProductWithTypeAndBrendSpecification(int id):base(c=>c.Id == id)
+        {
+            AddIncludes(x => x.ProductType);
+            AddIncludes(x => x.ProductBrand);
+        }
+    }
+}
--- e-commerce/e-commerce.Api/Controllers/ProductController.cs
+++ e-commerce/e-commerce.Api/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Infrastracure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,36 +13,42 @@ namespace e_commerce.Api.Controllers
     public class ProductController : ControllerBase
     {
 
-        private readonly IProductRepository _productRepository;
-        public ProductController(IProductRepository productRepository)
+        private readonly IGenericRepository<Products> _productRepository;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepository;
+        private readonly IGenericRepository<ProductType> _productTypeRepository;
+        public ProductController(IGenericRepository<Products> productRepository, IGenericRepository<ProductBrand> productBrandRepository, IGenericRepository<ProductType> productTypeRepository)
         {
             _productRepository = productRepository;
+            _productBrandRepository = productBrandRepository;
+            _productTypeRepository = productTypeRepository;
         }
         [HttpGet]
         public async Task<ActionResult<List<Products>>> GetProduct()
         {
-            var products = await _productRepository.GetProductsAsync();
+            var spec = new ProductWithTypeAndBrendSpecification();
+            var products = await _productRepository.ListAsync(spec);
             return Ok(products);
         }
 
         [HttpGet("types")]
         public async Task<ActionResult<List<ProductBrand>>> GetProductBrands()
         {
-            var productBrands = await _productRepository.GetProductsBrandsAsync();
+            var productBrands = await _productBrandRepository.ListAllAsync();
             return Ok(productBrands);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<List<ProductType>>> GetProductType()
         {
-            var productTypes = await _productRepository.GetProductsTypesAsync();
+            var productTypes = await _productTypeRepository.ListAllAsync();
             return Ok(productTypes);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Products>> GetProduct(int id)
         {
-            var product = await _productRepository.GetProductByIdAsync(id);
+            var spec = new ProductWithTypeAndBrendSpecification(id);
+            var product = await _productRepository.GetEntityWithSpec(spec);
             return Ok(product);
         }
     }
